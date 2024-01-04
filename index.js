e4f068834b8d13c0b32496af820b2cc844084f91
webfryingpan <vanyabrox2018@gmail.com>
2024-01-04 12:24:36 +0200

    functionality expansion

--- index.js
+++ index.js
@@ -8,32 +8,27 @@ export class DiscordClient {
 	}
 
 	setActivity() {
-		try {
-			this.rpc.setActivity({
-				details: this.activity.detail,
-				largeImageKey: this.activity.largeImageKey,
-				largeImageText: this.activity.largeImageText,
-				startTimestamp: new Date(),
-			})
-		} catch (err) {
-			console.error('Error setting activity:', err)
-		}
+		this.rpc.setActivity({
+			state: this.activity.state,
+			details: this.activity.detail,
+
+			largeImageKey: this.activity.largeImageKey,
+			largeImageText: this.activity.largeImageText,
+
+			smallImageKey: this.activity.smallImageKey,
+			smallImageText: this.activity.smallImageText,
+
+			buttons: this.activity.buttons,
+			startTimestamp: new Date(),
+		})
 	}
 
 	async login() {
-		try {
-			await this.rpc.login({ clientId: this.clientId })
-		} catch (err) {
-			console.error('Error logging in to Discord:', err)
-		}
+		await this.rpc.login({ clientId: this.clientId })
 	}
 
 	initialize() {
-		this.rpc.on('ready', () => {
-			console.log('Connected to Discord RPC')
-			this.setActivity()
-		})
-
+		this.rpc.on('ready', () => this.setActivity())
 		this.login()
 	}
 }
